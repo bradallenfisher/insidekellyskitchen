2580ebca77c2603e2812dbf2b1ea73aee9fa601d
Brad Fisher <bradallenfisher@gmail.com>
2018-05-30 08:36:05 -0400

    updates

--- sites/all/themes/kelly/templates/system/page.tpl.php
+++ sites/all/themes/kelly/templates/system/page.tpl.php
@@ -33,8 +33,8 @@
     </div>
 
     <div class="item">
-      <a href="https://inside-kellys-kitchen.myshopify.com/">
-       <img src="/sites/all/themes/kelly/images/store.jpg" />
+      <a href="/special-offers">
+       <img src="/sites/all/themes/kelly/images/callout.jpg" />
       </a>
     </div>
 
