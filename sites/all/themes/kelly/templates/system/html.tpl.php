8da131c66a264cf72c7a2e9339e1a9aee07129c8
Brad Fisher <bradallenfisher@gmail.com>
2019-03-30 11:12:44 -0400

    doh

--- sites/all/themes/kelly/templates/system/html.tpl.php
+++ sites/all/themes/kelly/templates/system/html.tpl.php
@@ -18,13 +18,13 @@
   <?php print $styles; ?>
   <?php print $scripts; ?>
 
-  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-  <script>
-    (adsbygoogle = window.adsbygoogle || []).push({
-      google_ad_client: "ca-pub-5270832306245806",
-      enable_page_level_ads: true
-    });
-  </script>
+  <!--<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <!--<script>
+  <!--  (adsbygoogle = window.adsbygoogle || []).push({
+  <!--    google_ad_client: "ca-pub-5270832306245806",
+  <!--    enable_page_level_ads: true
+  <!--  });
+  <!--</script>-->
 </head>
 <body<?php print $attributes;?>>
   <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
